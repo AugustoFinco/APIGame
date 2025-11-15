f4f77e577550d3550e4c6390440f38f74da7b470
AugustoFinco <augutoffavero@gmail.com>
2025-11-14 21:39:06 -0300

    commit 3

---
+++ API2/API2/Controllers/PlayerController.cs
@@ -0,0 +1,69 @@
+﻿using API2.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API2.Controllers
+{
+    public class PlayerController : ControllerBase
+    {
+        public static List<Player> players = new List<Player>() {
+            { new Player { id = "1", Vida = 100, Item = 0, posX = 0f, posY = 0f, posZ = 0f } }
+        };
+
+        [HttpGet]
+        [Route("usuariojogo")]
+        public IActionResult GetPlayers()
+        {
+            return Ok(players);
+        }
+
+        [HttpGet]
+        [Route("usuariojogo/{id}")]
+        public IActionResult GetPlayerByID(string id)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            return Ok(player);
+        }
+
+        [HttpPost]
+        [Route("usuariojogo")]
+        public IActionResult AddPlayer([FromBody] Player newPlayer)
+        {
+            players.Add(newPlayer);
+            return Ok(newPlayer);
+        }
+
+        [HttpPut]
+        [Route("usuariojogo/{id}")]
+        public IActionResult UpdatePlayer(string id, [FromBody] Player updatedPlayer)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            player.Vida = updatedPlayer.Vida;
+            player.Item = updatedPlayer.Item;
+            player.posX = updatedPlayer.posX;
+            player.posY = updatedPlayer.posY;
+            player.posZ = updatedPlayer.posZ;
+            return Ok(player);
+        }
+
+        [HttpDelete]
+        [Route("usuariojogo/{Id}")]
+        public IActionResult DeletePlayer(string id)
+        {
+            var player = players.FirstOrDefault(a => a.id == id);
+            if (player == null)
+            {
+                return NotFound();
+            }
+            players.Remove(player);
+            return Ok();
+        }
+    }
+}
---
+++ API2/API2/Model/Player.cs
@@ -0,0 +1,12 @@
+﻿namespace API2.Model
+{
+    public class Player
+    {
+        public string id { get; set; }
+        public int Item { get; set; }
+        public int Vida { get; set; }
+        public float posX { get; set; }
+        public float posY { get; set; }
+        public float posZ { get; set; }
+    }
+}
